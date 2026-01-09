9db591575008c8a82615818cb4485f6f4e9c1878
Wilk Maia <wilkmaia@gmail.com>
2026-01-09 08:30:01 -0300

    chore: add lualine.nvim plugin

---
+++ plugins/lualine.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+
+    -- @module 'lualine'
+    -- @type lualine.Config
+    opts = {
+      options = {
+        theme = 'onedark',
+        globalstatus = true,
+      },
+      sections = {
+        lualine_c = {
+          { 'filename', path = 1 },
+        },
+        lualine_x = {
+          'encoding',
+          'fileformat',
+          'filetype',
+          'lsp_status',
+        },
+        lualine_y = {
+          'progress',
+          'diff',
+          'diagnostics',
+        },
+      },
+      extensions = {
+        'fugitive',
+        'fzf',
+        'nerdtree',
+        'quickfix',
+        'trouble',
+      },
+    },
+  },
+}
