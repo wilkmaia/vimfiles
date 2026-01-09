0d0d560a759505a16815022fc6cd037a123ebd50
Wilk Maia <wilkmaia@gmail.com>
2026-01-09 08:29:21 -0300

    chore: trigger lsp autoformat on buffer write

--- events.lua
+++ events.lua
@@ -5,3 +5,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end
 })
+
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*',
+  group = vim.api.nvim_create_augroup('wim-buf-write-pre-autoformatting', { clear = true }),
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
+})
