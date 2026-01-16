090d4987fcbc7eeb55ed4148a37f8984c836f2e3
Wilk Maia <wilkmaia@gmail.com>
2026-01-16 08:23:18 -0300

    chore: pin lspkind.nvim version to avoid breaking change

--- plugins/blink.lua
+++ plugins/blink.lua
@@ -5,7 +5,7 @@ return {
     version = '1.*',
     lazy = 'VeryLazy',
     dependencies = {
-      { 'onsails/lspkind.nvim',        version = '*' },
+      { 'onsails/lspkind.nvim',        commit = '3ddd1b4edefa425fda5a9f95a4f25578727c0bb3' },
       { 'nvim-tree/nvim-web-devicons', version = '*' },
       { 'xzbdmw/colorful-menu.nvim',   version = '*' },
     },
